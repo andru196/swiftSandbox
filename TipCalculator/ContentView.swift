c6007cc2217ff0daa571c875afe6167420c052a0
andru196 <andru196@icloud.com>
2021-09-21 09:17:02 -0700

    init

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by andru on 21/09/2021.
+//  Copyright © 2021 NoOrganization. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,15 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by andru on 21/09/2021.
+//  Copyright © 2021 NoOrganization. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculator {
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+}
